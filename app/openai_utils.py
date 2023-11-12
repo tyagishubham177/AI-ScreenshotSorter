dc3b31df4e484e506dbae91737516e5f1ff60b20
shubham tyagi <shubham.tyagi@soprasteria.com>
2023-11-12 13:33:59 +0530

    text & vision APIs integrated

---
+++ app/openai_utils.py
@@ -0,0 +1,52 @@
+import openai
+import base64
+
+
+def get_image_description(api_key, image_path):
+    openai.api_key = api_key
+
+    try:
+        def encode_image(image_path):
+            with open(image_path, "rb") as image_file:
+                return base64.b64encode(image_file.read()).decode()
+
+        base64_image = encode_image(image_path)
+
+        response = openai.ChatCompletion.create(
+            model="gpt-4-vision-preview",
+            messages=[
+                {
+                    "role": "user",
+                    "content": [
+                        {
+                            "type": "image_url",
+                            "image_url": {
+                                "url": f"data:image/jpeg;base64,{base64_image}",
+                                "detail": "low"
+                            }
+                        }
+                    ]
+                }
+            ],
+            max_tokens=300
+        )
+        return response.choices[0].message.content
+    except Exception as e:
+        print(f"Error: {e}")
+        return f"An error occurred: {e}"
+
+
+def chat_with_openai(api_key, message, instructions=None):
+    openai.api_key = api_key
+
+    if instructions is None:
+        instructions = "You're a helpful assistant, adhere strictly to the prompt. Be exact in your answer."
+
+    response = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo",
+        messages=[
+            {"role": "system", "content": instructions},
+            {"role": "user", "content": message}
+        ]
+    )
+    return response.choices[0].message.content
--- app/routes.py
+++ app/routes.py
@@ -1,8 +1,36 @@
-from flask import Blueprint, request, jsonify
+from flask import Blueprint, jsonify, render_template
+from .openai_utils import get_image_description, chat_with_openai
+import os
 
-bp = Blueprint('routes', __name__)
+bp = Blueprint('routes', __name__, template_folder='templates')
 
 
 @bp.route('/')
 def index():
     return "Welcome to ScreenshotSorter!"
+
+
+@bp.route('/test-vision')
+def test_vision():
+    # Hardcoded path for testing
+    directory = r"C:\Users\Shubham Tyagi\Pictures\Screenshots"
+    file_name = "Screenshot (12).png"
+    image_path = os.path.join(directory, file_name)
+    openapi_key = "------ADD KEY-------"
+    description = get_image_description(openapi_key, image_path)
+
+    # Render the response in a simple HTML template
+    return render_template('response.html', title='Vision API Response', response=description)
+
+
+@bp.route('/test-chat')
+def test_chat():
+    # Hardcoded message for testing
+    message = "Translate 'Hello, world!' to Hindi"
+    openapi_key = "--------ADD KEY-------"
+    instructions = "Please translate the following text to Hindi accurately."
+
+    response = chat_with_openai(openapi_key, message, instructions)
+
+    # Render the response in a simple HTML template
+    return render_template('response.html', title='Chat API Response', response=response)
